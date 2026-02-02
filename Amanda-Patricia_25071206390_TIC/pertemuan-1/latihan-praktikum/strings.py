2447f3a3fd40e2f0e8e58180031948de1cef22e2
amandaP-93 <amandapatricia0610@gmail.com>
2026-02-03 00:35:56 +0700

    Add strings.py for string manipulation examples

---
+++ Amanda-Patricia_25071206390_TIC/pertemuan-1/latihan-praktikum/strings.py
@@ -0,0 +1,10 @@
+# strings.py
+# Contoh pengolahan string
+
+teks = "Belajar Python, lagii"
+
+print(teks)
+print(teks.upper())     # huruf besar
+print(teks.lower())     # huruf kecil
+print(teks[0])          # karakter pertama
+print(len(teks))        # panjang string
