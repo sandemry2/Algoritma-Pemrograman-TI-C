36ffcb28e3d257d53c70f4fdba48649051c8ba4a
adzandaffakarim <adzandaffa1416@gmail.com>
2026-02-03 21:01:57 +0700

    hello.py

---
+++ praktikum 1/hello.py
@@ -0,0 +1,8 @@
+print("heloo world")
+print("nama guah udin")
+nama = 'muhammad ihsan asshadiqi'
+kelas = 'teknik informatika C'
+nim = '25071206455'
+
+print('halo saya',nama,'saya di kelas teknik informatika C')
+print('udinn')
